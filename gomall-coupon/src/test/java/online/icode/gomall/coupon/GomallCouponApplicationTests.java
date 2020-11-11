52a263aaa95c32adbd031eeec03a1008bdc6c5a3
1435622547@qq.com <zcx950216>
2020-11-11 15:15:20 +0800

    初始化提交

---
+++ gomall-coupon/src/test/java/online/icode/gomall/coupon/GomallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package online.icode.gomall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GomallCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
